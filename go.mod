5c29ed5721863928fbef915d69507e4e133e9d18
Snir Yehuda <snir@savvy.security>
2023-08-27 21:03:06 +0000

    start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/snirye/golang-tcl-lsp
+
+go 1.19
+
+require github.com/alecthomas/participle/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
+)
+
+// AST represents the abstract syntax tree for a TCL program.
+type AST struct {
+	Statements []Statement
+}
+
+// Statement represents a statement in a TCL program.
+type Statement interface {
+	Statement()
+}
+
+// Command represents a command in a TCL program.
+type Command struct {
+	Name      string
+	Arguments []Expression
+}
+
+func (c *Command) Statement() {}
+
+// Expression represents an expression in a TCL program.
+type Expression interface {
+	Expression()
+}
+
+type UnquotedExpression struct {
+	Value string `@Ident`
+}
+
+func (UnquotedExpression) Expression() {}
+
+type Number struct {
+	Number float64 `@Float`
+}
+
+func (Number) Expression() {}
+
+type BracesGroup struct {
+	Value string `"{" @Ident "}"`
+}
+
+func (BracesGroup) Expression() {}
+
+type DoubleQuotesGroup struct {
+	String string `@String`
+}
+
+func (DoubleQuotesGroup) Expression() {}
+
+type SquareBracketsGroup struct {
+	Value string `"[" @Ident "]"`
+}
+
+func (SquareBracketsGroup) Expression() {}
+
+func main() {
+	// Parse a TCL program.
+	input := `
+set foo 123
+set bar "hello world"
+`
+
+	var lexer2 = lexer.Must(lexer.Rules{
+		"Root": {
+			{`BracesGroup`, `{`, lexer.Push("BracesGroup")},
+		},
+		"BracesGroup": {
+			{`BracesGroup`, `{`, lexer.Push("BracesGroup")},
+			{"BracesGroupEnd", `}`, lexer.Pop()},
+			{"Ident", `(.|\n)+`, nil},
+		},
+	})
+
+	parser, err := participle.Build[AST](
+		participle.Unquote("String"),
+		participle.Union[Expression](UnquotedExpression{}, Number{}, BracesGroup{}, DoubleQuotesGroup{}, SquareBracketsGroup{}),
+	)
+
+	// Access the parsed data.
+	for _, statement := range ast.Statements {
+		switch statement := statement.(type) {
+		case *Command:
+			fmt.Println("Command:", statement.Name, statement.Arguments)
+		case *Expression:
+			fmt.Println("Expression:", statement)
+		}
+	}
+}
